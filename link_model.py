8608298100b4dcbd3be497d34b08e7634d260f39
BOGY\Liraell <liraelle18@gmail.com>
2020-12-20 19:04:11 +0200

    Added calculating URL expiration date, manually entering URL lifetime in days, URL amount limit.

--- link_model.py
+++ link_model.py
@@ -1,7 +1,7 @@
 import sqlite3
 import string
 from random import choices
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from flask import request
 from db import db
 
@@ -10,11 +10,13 @@ class LinkModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     origin_link = db.Column(db.String(512))
     short_link = db.Column(db.String(4), unique=True)
-    created_date = db.Column(db.DateTime, default=datetime.now)
-    link_expiration = db.Column(db.Integer, default = 90)
+    created_date = db.Column(db.DateTime, default=date(2020, 4, 12))
+    #created_date = db.Column(db.DateTime, default=date.today)
+    link_expiration = db.Column(db.DateTime)
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, origin_link, link_expiration, **kwargs):
+        self.origin_link = origin_link
+        self.link_expiration = link_expiration
         self.short_link = self.gen_short_link()
 
     def gen_short_link(self):   
@@ -27,4 +29,20 @@ class LinkModel(db.Model):
             return self.gen_short_link()
         
         return (short_link)
+
+
+    def save_to_db(self):   # inserting and updating data to database
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_link(cls, short_link):
+        return cls.query.filter_by(short_link = short_link).first_or_404()
+
+    @classmethod
+    def delete_expired(cls, link_expiration):
+        limit = datetime.now() - timedelta(days=link_expiration)
+        cls.query.filter(LinkModel.created_date <= limit).delete()
+        db.session.commit()
+        return {'message'}
 
--- link_resource.py
+++ link_resource.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 from flask_restful import Resource, reqparse
 from flask import redirect, request
 from link_model import LinkModel
@@ -9,28 +10,35 @@ _link_parser.add_argument('URL',
                     type = str, 
                     required = True, 
                     help = "Paste your link here.")
-#_link_parser.add_argument('URL_expiration', 
-#                    type = int, 
-#                    required = False, 
-#                    help = "You can specify how many days link can live.")
+_link_parser.add_argument('URL_expiration', 
+                    type = int, 
+                    required = False, 
+                    help = "You can specify how many days link can live.")
 
 class Link(Resource):   # getting original URL from user
     def get(self):
         link = _link_parser.parse_args()
         
-        #if link['URL_expiration']:
-        #    item = LinkModel(origin_link=link['URL'])
-        #    item = LinkModel(link_expiration=link['URL_expiration'])
-        #else:
-        item = LinkModel(origin_link=link['URL'])   # creating short URL
+        counting = LinkModel.query.count()
+        if counting < 15:
+            if link['URL_expiration']:
+                #exp_date = datetime.date.today() + datetime.timedelta(days=link['URL_expiration'])
+                exp_date = datetime.date(2020, 4, 12) + datetime.timedelta(days=link['URL_expiration'])
+                item = LinkModel(link['URL'], exp_date) # creating short URL
+            else:
+                #exp_date = datetime.date.today() + datetime.timedelta(days=90)
+                exp_date = datetime.date(2020, 4, 12) + datetime.timedelta(days=90)
+                item = LinkModel(link['URL'], exp_date) # creating short URL
 
-        db.session.add(item)
-        db.session.commit()
+            item.save_to_db()
 
-        return {'short_link': item.short_link}
+            return {'short_link': request.base_url+item.short_link,
+                        'count': counting}
+        return {'message': 'database  is full'}
 
-class Short_link(Resource): #redirecting from short URL to original
+
+class Short_link(Resource): # redirecting from short URL to original
     def get(self, short_link):
-        url = LinkModel.query.filter_by(short_link=short_link).first_or_404()
+        url = LinkModel.find_link(short_link)
 
         return redirect(url.origin_link)
