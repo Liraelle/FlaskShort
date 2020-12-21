458d6c9c5754e9cf9c9d44b3c6ef154281e23b1e
BOGY\Liraell <liraelle18@gmail.com>
2020-12-21 19:13:46 +0200

    Added README, fix typo, code clean.

--- link_resource.py
+++ link_resource.py
@@ -19,9 +19,10 @@ _link_parser.add_argument('URL_expiration',
 class Link(Resource):   
     def get(self):
         link = _link_parser.parse_args()
+        db_limit = 1000000
 
         counting = LinkModel.query.count()
-        if counting >= 1000000: 
+        if counting >= db_limit: 
             exp_url = LinkModel.query.filter(LinkModel.link_expiration < datetime.date.today()).first()
             if exp_url:
                 exp_url.delete_expired()
@@ -30,6 +31,7 @@ class Link(Resource):
 
 
         if link['URL_expiration'] is None:
+            #exp_date = datetime.date(2020, 7, 12) + datetime.timedelta(days=90)
             exp_date = datetime.date.today() + datetime.timedelta(days=90)
             item = LinkModel(link['URL'], exp_date) # creating short URL with custom lifetime
 
@@ -37,20 +39,20 @@ class Link(Resource):
             return {'message': 'Incorrect value. It should be number from 1 to 365.'}
 
         else:
+            #exp_date = datetime.date(2020, 7, 12) + datetime.timedelta(days=link['URL_expiration'])
             exp_date = datetime.date.today() + datetime.timedelta(days=link['URL_expiration'])
-            item = LinkModel(link['URL'], exp_date) # creating short URL with standart lifetime
+            item = LinkModel(link['URL'], exp_date) # creating short URL with default lifetime
 
         item.save_to_db()
 
-        return {'short_link': request.base_url+item.short_link,
-                        'count': counting}
+        return {'short_link': request.base_url+item.short_link}
 
 
 # redirecting from short URL to original
 class Short_link(Resource): 
     def get(self, short_link):
         link = LinkModel.find_link(short_link)
-        if link.link_expiration < datetime.datetime.today():   # deleting URL if expired
+        if link.link_expiration < datetime.datetime.today():   # checking on expired short URL
             return {'message': 'URL expired.'}
 
         return redirect(link.origin_link)   # redirecting to original URL
---
+++ readme.md
@@ -0,0 +1,44 @@
+`FlaskShort` a test task REST API to generate short redirecting URL from original.
+To realize that task I used Flask, Flask-RESTful, SQLite3.
+
+The startup file is app.py. 
+In db.py we creating SQLAlchemy object to easily put/get information in/from database.
+
+For testing REST API I used Postman (v.7.36.1).
+
+**Usage**
+---
+
+For testing this API need to run API (using developer server, localhost). Than in Postman use method GET and type in field "Enter request URL" localhost:5000 (default port). In option "Body" choose "raw" and JSON format. In text field type, for example:
+```
+{
+    "URL": "https://stackoverflow.com/", 
+    "URL_expiration": 5 
+}
+```
+    in "URL" can be any URL, in "URL_expiration" can be number from 1 to 365 (days).
+    If you type:
+```
+{
+    "URL": "https://stackoverflow.com/" 
+}
+```
+    "URL_expiration" will be 90 days by default. After that press "Send". In Response window will be showing:
+```
+{
+    "short_link": "http://localhost:5000/Dm0K 
+}
+```
+
+After that needs to copy that short URL, paste in "Enter request URL", in "Body" choose "None" and press "Send". After that in Response window choose "Body", then "Preview". It should show page from original URL after redirecting from short URL.
+
+In API for simpler testing inside code I changed limit from 1 million to much less. (In link_resource.py, db_limit):
+```
+        db_limit = 1000000
+```
+
+For testing in case expired URL in database, I changing date on older (in link_resource.py, commented lines 33, 41), by commenting 32, 40 lines and uncomment 33, 41 lines:
+```
+            #exp_date = datetime.date(2020, 7, 12) + datetime.timedelta(days=90)
+            exp_date = datetime.date.today() + datetime.timedelta(days=90)
+```
